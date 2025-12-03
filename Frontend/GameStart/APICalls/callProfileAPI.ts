4ce179f6218a8ea17c9d4bb40e73aff3c0f5e8ea
Fazlul001 <fazlul.faizal.802@my.csun.edu>
2025-12-02 20:45:57 -0800

    changed the request url in callprofile

--- Frontend/GameStart/APICalls/callProfileAPI.ts
+++ Frontend/GameStart/APICalls/callProfileAPI.ts
@@ -6,7 +6,7 @@ const BACKEND_URL = "http://localhost:(PORT)/com.gamestart/v1/home/userauthentic
 
 // ⭐ NEW: helper function to call Python /login endpoint
 export async function loginWithEmailPassword(email: string, password: string) {
-  const res = await fetch(`${BACKEND_URL}/login`, {
+  const res = await fetch(`${BACKEND_URL}~${email}~${password}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",   // tell backend body is JSON
