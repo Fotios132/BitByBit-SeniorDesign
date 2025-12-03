737da99347c66ed71bb9232260314a8fd2db7626
Fazlul001 <fazlul.faizal.802@my.csun.edu>
2025-12-02 20:19:02 -0800

    API edits

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,5 +5,4 @@ def hello(request):
     return JsonResponse({"message": "Hello World"})
 
 def login(request, user_identifier):
-    print('Message Is', request.body)
     return JsonResponse(request.body)
